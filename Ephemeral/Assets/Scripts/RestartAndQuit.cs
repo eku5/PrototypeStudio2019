2f0bd855448d96801d5b5c7679475e8258439a35
Emperatriz <evee.ung@gmail.com>
2019-04-21 20:43:05 -0400

    Made a build

---
+++ Ephemeral/Assets/Scripts/RestartAndQuit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartAndQuit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
+        if (Input.GetKey("escape") || Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+    }
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("restartAndQuit");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+	
+    void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+        
+}
