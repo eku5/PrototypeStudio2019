d4d6aad0b68b9f1aa2c144047ec987da518cf051
Emperatriz <evee.ung@gmail.com>
2019-05-05 18:53:59 -0400

    Added more photos. Starting to divide them by theme.

---
+++ Vignette/Map/Assets/Scripts/RandQ.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandQ : MonoBehaviour
+{
+   void Update()
+       {
+           if (Input.GetKeyDown(KeyCode.R))
+           {
+               SceneManager.LoadScene(0);
+           }
+           
+           if (Input.GetKey("escape") || Input.GetKeyDown(KeyCode.Q))
+           {
+               Application.Quit();
+           }
+        }
+}
